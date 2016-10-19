9404b1c3a3a9e674504530721545676e72b9806b
RowanStrafford <rowan.strafford@ymail.com>
2016-10-19 23:41:14 +0100

    Added boosted speed to the player spaceship

--- Assets/Scripts/SpaceshipMovement.cs
+++ Assets/Scripts/SpaceshipMovement.cs
@@ -4,7 +4,10 @@ using System.Collections;
 public class SpaceshipMovement : MonoBehaviour {
 
     public float moveSpeed;
+    public float boostSpeed;
     public GameObject cameraObject;
+
+    private float speedUpTimer = 0f;
 
 	void Start ()
     {
@@ -15,12 +18,19 @@ public class SpaceshipMovement : MonoBehaviour {
     {
 	    if((Input.GetKey(KeyCode.W) || (Input.GetKey(KeyCode.UpArrow))))
         {
-            transform.Translate(Time.deltaTime * moveSpeed, 0, 0);
+            speedUpTimer += Time.deltaTime;
+            if(speedUpTimer > 1.0f)
+            {
+                transform.Translate(Time.deltaTime * boostSpeed, 0, 0);
+
+            } else transform.Translate(Time.deltaTime * moveSpeed, 0, 0);
         }
         else if ((Input.GetKey(KeyCode.S) || (Input.GetKey(KeyCode.DownArrow))))
         {
             transform.Translate(-Time.deltaTime * moveSpeed, 0, 0);
         }
+
+        if (Input.GetKeyUp(KeyCode.W) || (Input.GetKeyUp(KeyCode.UpArrow))) speedUpTimer = 0f;
 
         Vector3 mousePos = Input.mousePosition;
         Vector3 objectPos = Camera.main.WorldToScreenPoint(transform.position);
@@ -36,5 +46,5 @@ public class SpaceshipMovement : MonoBehaviour {
         if (transform.position.y > 25.0f) transform.position = new Vector3(transform.position.x, 25.0f, transform.position.z);
 
         cameraObject.transform.position = new Vector3(transform.position.x, transform.position.y, -10.0f);
-    }
+    }    
 }
