38b94410a461a1abd9f63e19a062240415f56fba
RowanStrafford <rowan.strafford@ymail.com>
2016-10-19 18:01:56 +0100

    Added rock object and script

---
+++ Assets/Scripts/RockBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RockBehaviour : MonoBehaviour {
+
+    public float rotateSpeed;
+
+	void Start ()
+    {
+	
+	}
+	
+	void Update ()
+    {
+        transform.Rotate(Time.deltaTime * rotateSpeed, Time.deltaTime * rotateSpeed, Time.deltaTime * rotateSpeed);
+	}
+}
---
+++ Assets/Scripts/RockSpawner.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RockSpawner : MonoBehaviour {
+
+    public GameObject player;
+    public GameObject rock;
+
+	void Start ()
+    {
+        Invoke("SpawnRocks", 4.0f);
+	}
+	
+	void Update ()
+    {
+	
+	}
+
+    void SpawnRocks()
+    {
+        //Vector3 playerPos = player.transform.position;
+
+        float randXValue = Random.Range(-30.0f, 30.0f);
+        float randYValue = Random.Range(-20.0f, 20.0f);
+
+        Vector3 spawnLocation = new Vector3(randXValue, randYValue, 0);
+
+        Instantiate(rock, spawnLocation, Quaternion.identity);
+    }
+}
