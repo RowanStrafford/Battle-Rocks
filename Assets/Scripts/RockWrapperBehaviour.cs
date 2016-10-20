a7af074f7375a42f8fbc89b6ced96f684aa748f3
Flash <flash.jaq@gmail.com>
2016-10-20 17:29:16 +0100

    Fixed Rocks

--- Assets/Scripts/RockWrapperBehaviour.cs
+++ Assets/Scripts/RockWrapperBehaviour.cs
@@ -6,13 +6,12 @@ public class RockWrapperBehaviour : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		//transform.position = new Vector3(transform.parent.position.x, transform.parent.position.y, 0);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		transform.Translate(Time.deltaTime * speed, 0, 0);
-
 	}
 
 	public void SetSpeed(float speed) {
