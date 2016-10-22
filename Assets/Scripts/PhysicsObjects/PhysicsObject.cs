677750cd1975e74ef573e94b2fa22fffaa0108f7
Flash <flash.jaq@gmail.com>
2016-10-22 23:45:55 +0100

    Reformatting - 1

---
+++ Assets/Scripts/PhysicsObjects/PhysicsObject.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class PhysicsObject : MonoBehaviour {
+
+	public Rigidbody rb;
+	protected float maxHealth;
+	protected float health;
+	public float healthMult;
+	public float density;
+	public float dmgMult;
+	public float maxVel;
+
+	// Use this for initialization
+	protected void Start() {
+		rb = GetComponent<Rigidbody>();
+		rb.SetDensity(density);
+		maxHealth = rb.mass * healthMult;
+		health = maxHealth;
+	}
+
+	protected void init() {
+
+	}
+
+	protected virtual void setVars() {
+
+	}
+
+	// Update is called once per frame
+	void Update() {
+
+	}
+
+	protected void FixedUpdate() {
+		Vector3 pos = transform.position;
+		pos.z = 0;
+		transform.position = pos;
+
+		if (rb.velocity.magnitude > maxVel)
+			rb.velocity = rb.velocity.normalized * maxVel;
+	}
+
+	void OnCollisionEnter(Collision col) {
+
+		float collisionForce = col.relativeVelocity.magnitude;
+		//Debug.Log(collisionForce);
+
+		PhysicsObject physicsObject = col.gameObject.GetComponent<PhysicsObject>();
+		physicsObject.takeDamage(collisionForce);
+
+	}
+
+	public void takeDamage(float damage) {
+		//Debug.Log(damage+" hp "+health);
+		health -= damage;
+		if (health <= 0)
+			Destroy(gameObject);
+	}
+
+	public float GetHealth() {
+		return health;
+	}
+
+	public void resetHealth() {
+		health = maxHealth;
+	}
+}
---
+++ Assets/Scripts/PhysicsObjects/Rock.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Rock : PhysicsObject {
+
+	//Physics
+	static float DEFAULT_DENSITY = 2f;
+
+
+	void Start ()
+    {
+		base.Start();
+		//Physics
+        //float meanRockSize = (transform.localScale.x + transform.localScale.y + transform.localScale.z) / 3;
+		
+		Debug.Log("Health: " + health + ", mass: " + rb.mass);
+	}
+
+	void Update() {	}
+
+	void FixedUpdate() {
+		base.FixedUpdate();
+	}
+
+    
+
+}
