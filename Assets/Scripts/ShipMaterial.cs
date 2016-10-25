98de43e3819d7b9b7d3fddf823ae27630e85f088
RowanStrafford <rowan.strafford@ymail.com>
2016-10-26 00:09:32 +0100

    Created Upgrade Bay Scene

---
+++ Assets/Scripts/ShipMaterial.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipMaterial : MonoBehaviour {
+
+    private Renderer rend;
+
+    private Texture textureUsed;
+
+	void Start ()
+    {
+        rend = GetComponent<Renderer>();
+	}
+
+	void Update ()
+    {
+	
+	}
+
+    public void changeTexture(Texture texture)
+    {
+        rend.material.mainTexture = texture;
+    }
+
+    public void SetTexture(Texture texture)
+    {
+        textureUsed = texture;
+    }
+}
