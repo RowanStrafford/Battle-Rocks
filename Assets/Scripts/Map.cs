38222528e09b969fa2c54cd8e1eab938884627f9
Flash <flash.jaq@gmail.com>
2016-10-29 21:14:47 +0100

    Changed the Background size/density

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -4,10 +4,10 @@ using UnityEngine.UI;
 
 public class Map : MonoBehaviour {
 
-	public static int X = -90;
-	public static int Y = -37;
-	public static int W = 180;
-	public static int H = 74;
+	public static int X = -160;
+	public static int Y = -107;
+	public static int W = 345;
+	public static int H = 195;
 
 	public static GameObject[] rocks;
 
